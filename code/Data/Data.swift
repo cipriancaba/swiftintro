6dc96de186d605e298f74bb23e167707c368c574
Ciprian Caba <caba.ciprian@hoyoslabs.com>
2015-08-28 23:13:25 +0300

    Added data objects

---
+++ code/Data/Data.swift
@@ -0,0 +1,18 @@
+//
+//  Data.swift
+//  TswiftDeck
+//
+//  Created by Ciprian Caba on 28/08/15.
+//  Copyright (c) 2015 Ciprian Caba. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class Data {
+  let json: JSON
+  
+  init(json: JSON) {
+    self.json = json
+  }
+}
